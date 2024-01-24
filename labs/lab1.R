ea51ab81b27c75a76b18da69a7eb7a0bf70365f5
Jonas Sanabria <112590439+JonasSanab@users.noreply.github.com>
2024-01-24 18:09:37 -0500

    lab1 updated
    exercises involving watertreament.csv and gwp3 data

--- labs/lab1.R
+++ labs/lab1.R
@@ -2,15 +2,15 @@ help(read.csv)
 theData <- read.csv("C:/Users/sanabj2/Dropbox/My PC (LAPTOP-8KJIP18D)/Desktop/Data Analytics/data_analytics_s2024/labs/2010EPI_data.csv", skip = 1, header = TRUE) #reads in csv file
 theData
 View(theData)
-attach(theData) #adds dataframe
-fix(theData) #editor
-EPI #Because we added a dataframe, this should print out theData$EPI
+df1 <- data.frame(theData) #adds dataframe
+fix(df1) #editor
+firstEPI <- df1$EPI
 
 
 #theOtherData <- read.csv(file.choose(), header=TRUE)
 
-tf <- is.na(EPI) # records True values if the value is NA
-E <- EPI[!tf] # filters out NA values, new array
+tf <- is.na(firstEPI) # records True values if the value is NA
+E <- firstEPI[!tf] # filters out NA values, new array
 E
 
 #Exercise 1
@@ -34,8 +34,8 @@ dsn")
 qqline(x)
 
 #second variable
-tf2 <- is.na(DALY) # records True values if the value is NA
-D <- DALY[!tf2]
+tf2 <- is.na(df1$DALY) # records True values if the value is NA
+D <- df1$DALY[!tf2]
 D
 fivenum(D,na.rm=TRUE)
 stem(D) #stem and leaf plot of EPI col
@@ -44,11 +44,77 @@ hist(D)
 #compare plots
 boxplot(E,D)
 qqplot(E,D)
+qqline(D, col = "blue") #i like blue
 
 
 #exercise 2
-EPILand<-EPI[!Landlock] #filters out values where Landlock is true/1
+EPILand<-df1$EPI[!df1$Landlock] #filters out values where Landlock is true/1
 ELand <- EPILand[!is.na(EPILand)] #filters out NA values
 hist(ELand)
 hist(ELand, seq(30., 95., 1.0), prob=TRUE) #histogram from 30 to 95 with intervals of 1
 
+
+#comparison between 2010 and 2016 EPI files
+newData <- read.csv("C:/Users/sanabj2/Dropbox/My PC (LAPTOP-8KJIP18D)/Desktop/Data Analytics/data_analytics_s2024/labs/EPI_data.csv", skip = 0, header = TRUE) #reads in csv file
+View(newData)
+df2 <- data.frame(newData)
+print(df2)
+df2$EPI
+tf2 <- is.na(df2$EPI) # records True values if the value is NA
+E2 <- df2$EPI[!tf2] # filters out NA values, new array
+E2
+E
+fivenum(E2)
+fivenum(E)
+
+#Filtering out Western Europe from newData EPI
+onlyWestEurope <- E2[df2$GEO_subregion == "Western Europe"]
+onlyWE <- onlyWestEurope[!is.na(onlyWestEurope)]
+hist(onlyWE)
+hist(E2)
+#compare no filter and only western europe
+boxplot(E2, onlyWE)
+fivenum(E2)
+fivenum(onlyWE)
+print(onlyWE)
+
+#GPW3_grump
+GPW3Data <- read.csv("C:/Users/sanabj2/Dropbox/My PC (LAPTOP-8KJIP18D)/Desktop/Data Analytics/data_analytics_s2024/labs/GPW3_GRUMP_SummaryInformation_2010.csv", skip = 0, header = TRUE) #reads in csv file
+View(GPW3Data)
+gdf <- data.frame(GPW3Data)
+lvUsed <- gdf$LevelUsed
+print(lvUsed)
+
+#get rid of NAs
+levels <- lvUsed[!is.na(lvUsed)]
+summary(levels)
+
+#values can only be integers for levelUsed
+#qqplot looks a little weird
+hist(levels)
+lines(density(levels,na.rm=TRUE,bw=1.))
+
+qqnorm(levels)
+qqline(levels)
+
+#filter out values with an area less than a certain threshold
+onlyBig <- levels[gdf$Area > 3000]
+summary(onlyBig)
+boxplot(levels,onlyBig)
+
+#watertreatment.csv
+waterData <- read.csv("C:/Users/sanabj2/Dropbox/My PC (LAPTOP-8KJIP18D)/Desktop/Data Analytics/data_analytics_s2024/labs/water-treatment.csv", skip = 0, header = TRUE) #reads in csv file
+View(waterData)
+wdf <- data.frame(waterData)
+php <- wdf$PH.P
+print(php)
+summary(php)
+hist(php)
+
+#lets filter for a specific range
+rangedPHP <- php[wdf$PH.P > 7.5 & wdf$PH.P < 8.1]
+summary(rangedPHP)
+
+boxplot(php,rangedPHP)
+qqnorm(rangedPHP)
+qqline(rangedPHP)
