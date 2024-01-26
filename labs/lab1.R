6a56db453b94a36caee7db3fa888b682c670fd58
Jonas Sanabria <112590439+JonasSanab@users.noreply.github.com>
2024-01-26 11:44:50 -0500

    lab 1 part 2 exercises
    continuation of lab 1

--- labs/lab1.R
+++ labs/lab1.R
@@ -118,3 +118,89 @@ summary(rangedPHP)
 boxplot(php,rangedPHP)
 qqnorm(rangedPHP)
 qqline(rangedPHP)
+
+#lab 1 part 2
+plot(ecdf(df1$EPI),do.points=FALSE,verticals = TRUE)
+plot(ecdf(df1$EPI),do.points=TRUE,verticals = TRUE)
+par(pty="s")
+help("qqnorm") # read the RStudio documentation for qqnorm
+help("qqplot") # read the RStudio documentation for qqplot
+qqnorm(df1$EPI)
+qqline(df1$EPI) # adding the line on the Q-Q plot
+x <- seq(30,95,1)
+x
+x2 <-seq(30,95,2)
+x2
+x2 <-seq(30,96,2)
+x2
+qqplot(qt(ppoints(250),df=5),x, xlab = "Q-Q plot")
+qqline(x)
+
+#data exploration for 1 other variable (i already did DALY previously)
+View(theData)
+summary(df1$WATER_H)
+plot(ecdf(df1$WATER_H),do.points=FALSE,verticals = TRUE)
+plot(ecdf(df1$WATER_H),do.points=TRUE,verticals = TRUE)
+qqnorm(df1$WATER_H)
+qqline(df1$WATER_H)
+
+#compare the multiple variables using a boxplot
+boxplot(df1$WATER_H, df1$DALY, df1$EPI)
+boxplot(df1$BIODIVERSITY, df1$ENVHEALTH, df1$ECOSYSTEM, df1$AIR_H, df1$AIR_E, df1$WATER_E)
+
+#book chapter exercises
+
+plot(cars$dist~cars$speed)
+plot(cars$dist~cars$speed,
+main="Relationship between car distance & speed",
+xlab="Speed (miles per hour)",
+ylab="Distance travelled (miles)",
+xlim=c(0,30),
+ylim=c(0,140),
+xaxs="i",
+yaxs="i",
+col="red",
+pch=19)
+
+plot(mtcars$wt,mtcars$mpg)
+#after installing proper packages
+library(ggplot2)
+qplot(mtcars$wt,mtcars$mpg)
+qplot(wt,mpg,data = mtcars)
+ggplot(mtcars,aes(x=wt,y=mpg))+geom_point()
+plot(pressure$temperature,pressure$pressure, type = "l")
+points(pressure$temperature,pressure$pressure)
+
+lines(pressure$temperature,pressure$pressure/2, col="red")
+points(pressure$temperature,pressure$pressure/2, col="blue")
+
+qplot(pressure$temperature,pressure$pressure, geom="line")
+qplot(temperature,pressure, data = pressure, geom="line")
+ggplot(pressure, aes(x = temperature,y=pressure)) +geom_line() + geom_point()
+ 
+
+
+barplot(BOD$demand,names.arg = BOD$Time)
+table(mtcars$cyl)
+barplot(table(mtcars$cyl))
+qplot(mtcars$cyl)
+qplot(factor(mtcars$cyl))
+qplot(factor(cyl),data=mtcars)
+ggplot(mtcars,aes(x=factor(cyl))) + geom_bar()
+
+mtc <- mtcars$mpg
+hist(mtc)
+hist(mtc, breaks = 10)
+hist(mtc, breaks= 5)
+hist(mtc, breaks = 12)
+qplot(mpg, data = mtcars, binwidth = 4)
+ggplot(mtcars,aes(x=mpg)) + geom_histogram(binwidth = 4)
+ggplot(mtcars,aes(x=mpg)) + geom_histogram(binwidth = 5)
+
+plot(ToothGrowth$supp, ToothGrowth$len)
+boxplot(len ~ supp, data = ToothGrowth)
+boxplot(len ~ supp + dose, data = ToothGrowth)
+qplot(ToothGrowth$supp,ToothGrowth$len, geom = "boxplot")
+ggplot(ToothGrowth, aes(x=supp,y=len)) + geom_boxplot()
+qplot(interaction(ToothGrowth$supp,ToothGrowth$dose), ToothGrowth$len, geom = "boxplot")
+ggplot(ToothGrowth, aes(x=interaction(supp,dose), y=len)) + geom_boxplot()
