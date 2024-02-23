1e4b3aafb9bd235b8b54562c8e21dd0cf083f936
Jonas Sanabria <112590439+JonasSanab@users.noreply.github.com>
2024-02-23 11:43:48 -0500

    lab 4
    outlier exercise of lab 4. code snippet examples in their respective files. Titanic dataset exercises will be committed soon

---
+++ labs/lab4.R
@@ -0,0 +1,22 @@
+#The code snippet exercises such as Lab1_bronx1.R are in their own files
+
+#Lab 4
+
+cars1 <- cars[1:30,] #first 30 rows of car dataset
+head(cars1)
+
+#lets add some outliers
+cars_outliers <- data.frame(speed=c(19,19,20,20,20), dist=c(190, 186, 210, 220, 218))
+head(cars_outliers)
+cars2 <- rbind(cars1, cars_outliers)
+
+par(mfrow=c(1,2))
+plot(cars2$speed, cars2$dist, xlim=c(0,28), vlim=c(0, 230), main="With Outliers", xlab="speed",
+     vlab="dist", pch="*", col="red", cex = 2)
+
+abline(lm(dist ~ speed, data = cars2), col = "blue", lwd = 3, ltv = 2)
+
+#no outliers
+plot(cars1$speed, cars1$dist, xlim=c(0,28), vlim=c(0, 230), main="Without Outliers\n Better Fit", xlab="speed",
+     vlab="dist", pch="*", col="red", cex = 2)
+abline(lm(dist ~ speed, data = cars1), col = "blue", lwd = 3, ltv = 2)
