7d2c9f2d67f795d8cb746998ad0b1e0b4761c825
Jonas Sanabria <112590439+JonasSanab@users.noreply.github.com>
2024-03-29 11:13:52 -0400

    finished PCA exercises
    PCA exercises completed

--- labs/PCAexerciseinclass.R
+++ labs/PCAexerciseinclass.R
@@ -45,3 +45,58 @@ pca_out <- prcomp(Boston, scale = T)
 pca_out
 
 plot(pca_out)
+
+biplot(pca_out, scale = 0)
+boston_pc <- pca_out$x
+boston_pc
+
+head(boston_pc)
+summary(boston_pc)
+
+
+#USArrests
+#50 states are the rows
+data(USArrests)
+View(USArrests)
+states = row.names(USArrests)
+states
+names(USArrests)
+
+apply(USArrests, 2, mean)
+apply(USArrests, 2, var)
+
+# We now perform principal components analysis using the prcomp() function, which is one of several functions in R that perform PCA.
+# By default, the prcomp() function centers the variables to have mean zero. By using the option scale=TRUE,
+# we scale the variables to have standard deviation one.
+# The output from prcomp() contains a number of useful quantities.
+pr.out=prcomp(USArrests, scale=TRUE)
+
+names(pr.out)
+
+# The center and scale components correspond to the means and standard deviations of the
+# variables that were used for scaling prior to implementing PCA.
+pr.out$center
+pr.out$scale
+
+
+#The rotation matrix provides the principal component loadings; each column of pr.out$rotation contains
+#the corresponding principal component loading vector
+#We see that there are four distinct principal components.
+pr.out$rotation
+
+dim(pr.out$x)
+
+biplot(pr.out, scale = 0)
+pr.out$sdev
+
+
+#The variance explained by each principal component is obtained by squaring these:
+pr.var = pr.out$sdev^2
+pr.var
+
+
+#To compute the proportion of variance explained by each principal component, we simply
+#divide the variance explained by each principal component by the total variance explained
+#by all four principal components.
+pve = pr.var/sum(pr.var)
+pve
